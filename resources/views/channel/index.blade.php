4be6c774c482597d896a3b001e67c129fec6101e
Riswan Ardinata <68963468+riswanardinatahub@users.noreply.github.com>
2021-06-26 11:19:24 +0700

    banner

--- resources/views/channel/index.blade.php
+++ resources/views/channel/index.blade.php
@@ -10,7 +10,7 @@
 
 <!-- start banner Area -->
 			<section class="banner-area relative about-banner" id="home">	
-				<div class="overlay overlay-bg"></div>
+				<div class=""></div>
 				<div class="container">				
 					<div class="row d-flex align-items-center justify-content-center">
 						<div class="about-content col-lg-12">
