afb40ee8f8fdcd5800967dae184a6f7d265f1dcb
Riswan Ardinata <68963468+riswanardinatahub@users.noreply.github.com>
2021-06-27 10:40:14 +0700

    test permission

--- config/filesystems.php
+++ config/filesystems.php
@@ -52,6 +52,16 @@ return [
         'videos' => [
             'driver' => 'local',
             'root' => storage_path('app/videos'),
+            'permissions' => [
+                                'file' => [
+                                    'public' => 0664,
+                                    'private' => 0600,
+                                ],
+                                'dir' => [
+                                    'public' => 0775,
+                                    'private' => 0700,
+                                ],
+                            ],
             'url' => env('APP_URL'),
             'visibility' => 'public',
         ],
